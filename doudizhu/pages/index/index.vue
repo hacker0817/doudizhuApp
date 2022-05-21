7ab31fe7f2fe8f66c00a8c26d56176126aafc36c
渺小的程序员 <magicwuyujun@126.com>
2022-05-22 01:20:31 +0800

    移动端单机版

---
+++ doudizhu/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<web-view :src="url"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: 'http://192.168.0.212/web/doudizhuClient/index.html'
+			}
+		},
+		onLoad(options) {
+			if (options && options.url) {
+				this.url = options.url;
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ doudizhu/unpackage/dist/dev/app-plus/app-service.js
@@ -0,0 +1,131 @@
+if (typeof Promise !== "undefined" && !Promise.prototype.finally) {
+  Promise.prototype.finally = function(callback) {
+    const promise = this.constructor;
+    return this.then((value) => promise.resolve(callback()).then(() => value), (reason) => promise.resolve(callback()).then(() => {
+      throw reason;
+    }));
+  };
+}
+;
+if (typeof uni !== "undefined" && uni && uni.requireGlobal) {
+  const global = uni.requireGlobal();
+  ArrayBuffer = global.ArrayBuffer;
+  Int8Array = global.Int8Array;
+  Uint8Array = global.Uint8Array;
+  Uint8ClampedArray = global.Uint8ClampedArray;
+  Int16Array = global.Int16Array;
+  Uint16Array = global.Uint16Array;
+  Int32Array = global.Int32Array;
+  Uint32Array = global.Uint32Array;
+  Float32Array = global.Float32Array;
+  Float64Array = global.Float64Array;
+  BigInt64Array = global.BigInt64Array;
+  BigUint64Array = global.BigUint64Array;
+}
+;
+if (uni.restoreGlobal) {
+  uni.restoreGlobal(Vue, weex, plus, setTimeout, clearTimeout, setInterval, clearInterval);
+}
+(function(vue, shared) {
+  "use strict";
+  var _export_sfc = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+      target[key] = val;
+    }
+    return target;
+  };
+  const _sfc_main$1 = {
+    data() {
+      return {
+        url: "http://192.168.0.212/web/doudizhuClient/index.html"
+      };
+    },
+    onLoad(options) {
+      if (options && options.url) {
+        this.url = options.url;
+      }
+    },
+    methods: {}
+  };
+  function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+    return vue.openBlock(), vue.createElementBlock("view", null, [
+      vue.createElementVNode("web-view", { src: $data.url }, null, 8, ["src"])
+    ]);
+  }
+  var PagesIndexIndex = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__file", "E:/GitCode/doudizhu/pages/index/index.vue"]]);
+  __definePage("pages/index/index", PagesIndexIndex);
+  function isDebugMode() {
+    return typeof __channelId__ === "string" && __channelId__;
+  }
+  function jsonStringifyReplacer(k, p) {
+    switch (shared.toRawType(p)) {
+      case "Function":
+        return "function() { [native code] }";
+      default:
+        return p;
+    }
+  }
+  function normalizeLog(type, filename, args) {
+    if (isDebugMode()) {
+      args.push(filename.replace("at ", "uni-app:///"));
+      return console[type].apply(console, args);
+    }
+    const msgs = args.map(function(v) {
+      const type2 = shared.toTypeString(v).toLowerCase();
+      if (["[object object]", "[object array]", "[object module]"].indexOf(type2) !== -1) {
+        try {
+          v = "---BEGIN:JSON---" + JSON.stringify(v, jsonStringifyReplacer) + "---END:JSON---";
+        } catch (e) {
+          v = type2;
+        }
+      } else {
+        if (v === null) {
+          v = "---NULL---";
+        } else if (v === void 0) {
+          v = "---UNDEFINED---";
+        } else {
+          const vType = shared.toRawType(v).toUpperCase();
+          if (vType === "NUMBER" || vType === "BOOLEAN") {
+            v = "---BEGIN:" + vType + "---" + v + "---END:" + vType + "---";
+          } else {
+            v = String(v);
+          }
+        }
+      }
+      return v;
+    });
+    return msgs.join("---COMMA---") + " " + filename;
+  }
+  function formatAppLog(type, filename, ...args) {
+    const res = normalizeLog(type, filename, args);
+    res && console[type](res);
+  }
+  const _sfc_main = {
+    onLaunch: function() {
+      formatAppLog("log", "at App.vue:4", "App Launch");
+      plus.screen.lockOrientation("landscape-primary");
+    },
+    onShow: function() {
+      formatAppLog("log", "at App.vue:8", "App Show");
+    },
+    onHide: function() {
+      formatAppLog("log", "at App.vue:11", "App Hide");
+    }
+  };
+  var App = /* @__PURE__ */ _export_sfc(_sfc_main, [["__file", "E:/GitCode/doudizhu/App.vue"]]);
+  function createApp() {
+    const app = vue.createVueApp(App);
+    return {
+      app
+    };
+  }
+  const { app: __app__, Vuex: __Vuex__, Pinia: __Pinia__ } = createApp();
+  uni.Vuex = __Vuex__;
+  uni.Pinia = __Pinia__;
+  __app__.provide("__globalStyles", __uniConfig.styles);
+  __app__._component.mpType = "app";
+  __app__._component.render = () => {
+  };
+  __app__.mount("#app");
+})(Vue, uni.VueShared);
